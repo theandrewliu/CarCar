128f871a6aba9f34bb2aa1ec38ef460e0ea2d6f6
Andrew <andrew.liu.722@gmail.com>
2022-05-09 13:37:42 -0700

    added to settings and urls in service project

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .models import
+from common.json import ModelEncoder
+import json
 
 # Create your views here.
