3f9196065dbe4cc5ac7336ef8a8b22710f47311e
Andrew <andrew.liu.722@gmail.com>
2022-05-11 16:05:11 -0700

    fixed poller for sales

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -18,7 +18,7 @@ def poll():
     while True:
         print('Sales poller polling for data')
         try:
-            response = requests.get("http://inventory-api:8100/api/automobile")
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
             content = json.loads(response.content)
             for automobile in content["automobile"]:
                 AutomobileVO.objects.update_or_create(
